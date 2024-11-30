438567e7cc3078d9ad39715625e089a9cdb92988
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-30 18:49:09 +0100

    Client Side Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>MERN Stack Blog</h1>
+      <p>MongoDB, Express.js, React.js, Node.js</p>
+    </div>
+  )
+}
+
+export default App
