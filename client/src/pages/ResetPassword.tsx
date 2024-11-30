09a65eadb0d5946429cd2cac9edfe4af61ad9701
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-30 20:16:13 +0100

    Server Setup Edit

--- client/src/pages/ResetPassword.tsx
+++
@@ -1,7 +0,0 @@
-const ResetPassword = () => {
-    return (
-        <div>Reset Password Page</div>
-    );
-}
-
-export default ResetPassword;
--- client/src/pages/SignIn.tsx
+++
@@ -1,7 +0,0 @@
-const SignIn = () => {
-    return (
-        <div>Sign In Page</div>
-    );
-}
-
-export default SignIn;
